c9977c99e0954ac98eb3e4fdb9a3481726b74f28
Ricou107 <76947043+Ricou107@users.noreply.github.com>
2022-12-22 15:05:25 +0100

    integrate 404 page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,16 +1,16 @@
 import { useRouteError } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './Error.css'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="error-page">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/">Retourner sur la page d’accueil</NavLink>
     </div>
   );
 }
